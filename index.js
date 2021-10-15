36f1212fbb5a318c45098a84dc6361edf2b644c3
agiraldo053 <agiraldo053@gmail.com>
2021-10-15 13:58:05 -0500

    se configuro heroku

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ const apiRouter=require('./routes');
 
 app.use(morgan('dev'));
 app.use(cors());
+app.use((req, res, next)=>{
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, ContentType, Accept");
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+ });
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 //mongodb local
